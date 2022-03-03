124c982e9a812f3406fec8355775a0e4a85fdc74
Shu-Huai Sean Lin <slin185@terpmail.umd.edu>
2022-03-03 02:49:58 -0500

    added load()

--- task1/main.cpp
+++ task1/main.cpp
@@ -3,25 +3,32 @@ using namespace std;
 
 int main () {
     // string s = "10010111010010100000000000000000";
-    string s = "1001011101001010";
+    // string s = "1001011101001010";
+    string s = "1111111111111111";
     reverse(s.begin(), s.end());
 
     // Flip bits since bitset accesses values from lowest to greatest bit
-    std::bitset<N> bs(s);
+    std::bitset<SIZE> bs(s);
 
     cout << "Bitvector: " << bs.to_string() << endl;
 
     rank_support r(&bs);
     int i;
-    for (i = 1; i <= N; i++) 
+    for (i = 1; i <= SIZE; i++) 
     {
       cout << "Rank " << i << ": " << r.rank1(i) << endl;
     }
-
     cout << "Overhead (in bits): " << r.overhead() << endl;
 
     string file = "test";
-    r.save(file);
+    // r.save(file);
+    r.load(file);
+
+    for (i = 1; i <= SIZE; i++) 
+    {
+      cout << "Rank " << i << ": " << r.rank1(i) << endl;
+    }
+    cout << "Overhead (in bits): " << r.overhead() << endl;
 
     return 0;
 }
--- task1/rank_support.h
+++ task1/rank_support.h
@@ -3,26 +3,24 @@
 
 #include <bit> // For popcount
 #include <bitset> // For bit vectors
-#include <iostream> // For save and load files
-#include <fstream> // For save and load files
+#include <iostream> // For save and load function
+#include <fstream> // For save and load function
+#include <sstream> // For load function
 #include <math.h> // For log2 function
 #include <algorithm> // For reversing bit string
 
 using namespace std;
 
 // Modify N to change size of bit vector
-const int N = 16;
+const int SIZE = 16;
 
 class rank_support {
-  bitset<N>* b_;
+  bitset<SIZE>* b_;
   uint64_t* Rs_;
   uint64_t* Rb_;
-  float N_;
-  float S_;
-  float B_;
 
 public:
-  rank_support(bitset<N>* b);
+  rank_support(bitset<SIZE>* b);
   ~rank_support();
   uint64_t rank1(uint64_t i);
   uint64_t overhead();
